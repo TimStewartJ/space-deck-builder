fae89eb58c98c9473918e5b34de9feb1d32fffa1
Tim Stewart <timstewartj@gmail.com>
2025-04-09 17:49:03 -0700

    Refactor logging functions to ensure messages are logged only when verbose mode is enabled

--- simulator/src/utils/logger.py
+++ simulator/src/utils/logger.py
@@ -1,11 +1,47 @@
-def log_to_console(message):
-    print(message)
+verbose = True  # Global flag to control logging
 
-def log_to_file(message, filename='game_log.txt'):
-    with open(filename, 'a') as log_file:
-        log_file.write(message + '\n')
+def set_verbose(value):
+    """
+    Set the verbose flag to control logging output.
+    
+    Args:
+        value (bool): Whether to enable verbose logging
+    """
+    global verbose
+    verbose = value
 
-def log_event(event_type, details):
-    message = f"{event_type}: {details}"
+def log(message):
+    """
+    Log a message to the console and a file.
+    
+    Args:
+        message (str): The message to log.
+    """
     log_to_console(message)
     log_to_file(message)
+
+def log_verbose(message):
+    """
+    Log a message if verbose mode is enabled.
+    
+    Args:
+        message (str): The message to log.
+    """
+    if verbose:
+        log_to_console(message)
+        log_to_file(message)
+
+def log_to_console(message):
+    if verbose:
+        print(message)
+
+def log_to_file(message, filename='game_log.txt'):
+    if verbose:
+        with open(filename, 'a') as log_file:
+            log_file.write(message + '\n')
+
+def log_event(event_type, details):
+    if verbose:
+        message = f"{event_type}: {details}"
+        log_to_console(message)
+        log_to_file(message)
