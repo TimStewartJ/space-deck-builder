6942dc9ab6a2e78c873ea831eae91087ae8ac702
Tim Stewart <timstewartj@gmail.com>
2025-04-22 17:27:18 -0700

    make random agent way smarter

--- src/ai/random_agent.py
+++ src/ai/random_agent.py
@@ -1,6 +1,6 @@
 from random import choice
 from src.engine.game import Game
-from src.engine.actions import get_available_actions
+from src.engine.actions import ActionType, get_available_actions
 from src.ai.agent import Agent
 
 class RandomAgent(Agent):
@@ -14,6 +14,16 @@ class RandomAgent(Agent):
 
         if len(available_actions) == 1:
             return available_actions[0]
+
+        # Randomly pick from play card actions first
+        play_card_actions = [action for action in available_actions if action.type == ActionType.PLAY_CARD]
+        if play_card_actions:
+            return choice(play_card_actions)
+        
+        # Then pick attack player if available
+        attack_player_actions = [action for action in available_actions if action.type == ActionType.ATTACK_PLAYER]
+        if attack_player_actions:
+            return choice(attack_player_actions)
         
         if self.allow_end_turn:
             return choice(available_actions)
